45a6d36297a016477e3d98cb9d586cd1e373e3d2
Morgan Ling <morganhz.ling@gmail.com>
2017-04-22 21:26:43 -0700

    wrote the FetchData function in SwipeActivity. Does Yelp API calls using asynctask

--- app/src/main/java/com/mandarker/feed/SwipeActivity.java
+++ app/src/main/java/com/mandarker/feed/SwipeActivity.java
@@ -1,24 +1,64 @@
 package com.mandarker.feed;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yelp.fusion.client.connection.YelpFusionApi;
 import com.yelp.fusion.client.connection.YelpFusionApiFactory;
+import com.yelp.fusion.client.models.Business;
+import com.yelp.fusion.client.models.SearchResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class SwipeActivity extends AppCompatActivity {
     YelpFusionApiFactory apiFactory = new YelpFusionApiFactory();
     YelpFusionApi yelpFusionApi;
-
+    Map<String, String> params;
+    Call<SearchResponse> call;
+    SearchResponse searchResponse;
+    ArrayList<Business> businessList;
+    int numOfResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
-            yelpFusionApi = apiFactory.createAPI(getString(R.string.appID),getString(R.string.appSecret));
+            yelpFusionApi = apiFactory.createAPI(getString(R.string.appID), getString(R.string.appSecret));
+        } catch (Exception e) {
         }
-        catch(Exception e){}
+        params = new HashMap<>();
+        params.put("term", "rice");
+        new FetchData().execute();
+    }
 
+    public class FetchData extends AsyncTask<String, String, String> {
 
+        @Override
+        protected String doInBackground(String... strings) {
+            call = yelpFusionApi.getBusinessSearch(params);
+            try {
+                searchResponse = call.execute().body();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (searchResponse != null) {
+                numOfResponse = searchResponse.getTotal();
+                businessList = searchResponse.getBusinesses();
+                for(int i = 0; i < numOfResponse; i++)
+                {
+                    System.out.println("Business name: " + businessList.get(i).getName());
+                }
+                System.out.println("Number of responses: " + numOfResponse + " ");
+            }
+            return null;
+        }
     }
 }
+
