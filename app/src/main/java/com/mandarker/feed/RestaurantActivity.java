cf4a170a5f85eb661893b62a266ff47cdaafe5ec
mandarker <springnguyen98@gmail.com>
2017-04-22 21:23:59 -0700

    Set up restaurant object reference in RestaurantActivity

--- app/src/main/java/com/mandarker/feed/RestaurantActivity.java
+++ app/src/main/java/com/mandarker/feed/RestaurantActivity.java
@@ -3,11 +3,20 @@ package com.mandarker.feed;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mandarker.feed.classes.Restaurant;
+
 public class RestaurantActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        Restaurant restaurant;
+
+        Bundle bundle = this.getIntent().getExtras();
+        if (bundle != null){
+            restaurant = bundle.getParcelable("restaurant");
+        }
     }
 }
