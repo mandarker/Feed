129c76718ba3133d5922f98e9e2859bb5f432c9a
Morgan Ling <morganhz.ling@gmail.com>
2017-04-22 18:33:06 -0700

    changed MainActivity to SwipeActivity added Yelp API

--- app/src/main/java/com/mandarker/feed/SwipeActivity.java
+++ app/src/main/java/com/mandarker/feed/SwipeActivity.java
@@ -6,13 +6,20 @@ import android.os.Bundle;
 import com.yelp.fusion.client.connection.YelpFusionApi;
 import com.yelp.fusion.client.connection.YelpFusionApiFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class SwipeActivity extends AppCompatActivity {
     YelpFusionApiFactory apiFactory = new YelpFusionApiFactory();
-    YelpFusionApi yelpFusionApi = apiFactory.createAPI(appId, appSecret);
+    YelpFusionApi yelpFusionApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            yelpFusionApi = apiFactory.createAPI(getString(R.string.appID),getString(R.string.appSecret));
+        }
+        catch(Exception e){}
+
+
     }
+
 }
