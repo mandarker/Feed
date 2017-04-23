51c2b24d027baef295d9a09d58f92bf6d321eb76
mandarker <springnguyen98@gmail.com>
2017-04-22 18:06:05 -0700

    Add Restaurant and OnSwipeTouchListener classes

--- app/src/main/java/com/mandarker/feed/MainActivity.java
+++ app/src/main/java/com/mandarker/feed/MainActivity.java
@@ -3,12 +3,14 @@ package com.mandarker.feed;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mandarker.feed.classes.OnSwipeTouchListener;
 import com.yelp.fusion.client.connection.YelpFusionApi;
 import com.yelp.fusion.client.connection.YelpFusionApiFactory;
 
 public class MainActivity extends AppCompatActivity {
+
     YelpFusionApiFactory apiFactory = new YelpFusionApiFactory();
-    YelpFusionApi yelpFusionApi = apiFactory.createAPI(appId, appSecret);
+    //YelpFusionApi yelpFusionApi = apiFactory.createAPI(appId, appSecret);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
